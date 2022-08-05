8353245da76265d24001046a0eb78472f87b3470
Tatiana <hina_miste@mail.ru>
2022-08-05 18:31:58 +0300

    add Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает 
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+while (counter <= N)
+{
+    if (counter % 2 == 0)
+    {
+        if (counter == 2)
+        {
+            Console.Write(counter);
+        }
+        else
+        {
+            Console.Write("," + counter);
+        }
+    }
+    counter = counter + 1;
+}
