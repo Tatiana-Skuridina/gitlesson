277cf1ad3dbe01f8a6080e4e0551d8a8d05d5796
Tatiana <hina_miste@mail.ru>
2022-08-13 13:24:42 +0300

    .\Task10\

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите трехзначное число ");
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число ");
 int number = Convert.ToInt32(Console.ReadLine());
 int[] nums = {(number / 100), (number / 10 % 10), (number % 10)};
 Console.WriteLine(nums[1]);
