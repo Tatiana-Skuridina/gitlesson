6d0ccc022bd7231afa5282ed7b79bd43be6c2edb
Tatiana <hina_miste@mail.ru>
2022-08-20 17:55:55 +0300

    add task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+Console.WriteLine("Введите массив чисел:");
+string number = Console.ReadLine();
+string[] array = new string[number.Length];
+int index = 0;
+foreach (var item in number)
+    {
+        if(item != ',')
+        {
+            array[index] += item.ToString();
+        }
+        else
+        { 
+            index++;
+        }
+    }
+Console.Write("[ ");
+for(int i = 0; i<=index-1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+Console.Write(array[index] + " ]");
