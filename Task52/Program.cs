8485db511c45cda3d7822ce998fbbe5ec3609381
Tatiana <hina_miste@mail.ru>
2022-08-28 21:12:58 +0300

    add task 52

---
+++ Task52/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0,10);
+        }
+    }
+}
+
+void FindMedium(double result, int[,] matr)
+{
+    for (int i = 0; i <= matr.GetLength(0); i++) 
+    {
+        result = 0;
+        for (int j = 0; j < matr.GetLength(1)-1; j++)
+        {
+            result = result + Convert.ToDouble(matr[j,i]);
+        }
+        Console.Write("{0:0.00};", (result/3));
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
+double x = 0;
+Console.WriteLine();
+FindMedium(x, matrix);
