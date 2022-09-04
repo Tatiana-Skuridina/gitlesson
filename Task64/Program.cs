20b5ec87e0027ac0bcc16f8b79545d8da6c71fb3
Tatiana <hina_miste@mail.ru>
2022-09-04 13:13:27 +0300

    add task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.WriteLine("Введите число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N > M:");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N - M + 1];
+Console.WriteLine("Все натуральные числа от M до N:");
+FillArray(array);
+
+void FillArray(int[] matr)
+{
+    matr[0] = M;
+    Console.Write(matr[0] + " ");
+    for (int i = 1; i < matr.Length; i++)
+    {
+        matr[i] = matr[i - 1] + 1;
+        Console.Write(matr[i] + " ");
+    }
+}
