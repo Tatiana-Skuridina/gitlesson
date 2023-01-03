939d302d6001c7de10831324972e4159639ea48e
Tatiana <hina_miste@mail.ru>
2023-01-03 18:37:13 +0300

    changed Project10, added TelegramBot

--- Project10/main.py
+++ Project10/main.py
@@ -1,4 +1,13 @@
-from isOdd import isOdd
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from bot_commands import *
 
-print(isOdd(0)) 
-print(isOdd(4)) 
+app = Updater("5933770303:AAE_XUo7OCggKXAjSVVrJfyRYX3tbQ-h5mk")
+
+app.dispatcher.add_handler(CommandHandler("hello", hello_command))
+app.dispatcher.add_handler(CommandHandler("time", time_command))
+app.dispatcher.add_handler(CommandHandler("help", help_command))
+app.dispatcher.add_handler(CommandHandler("sum", sum_command))
+
+app.start_polling()
+app.idle()
